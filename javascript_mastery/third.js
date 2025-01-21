aa9e1e4056ad94fd12a52b2cf13447e813f6eb24
CSE66 <cse.23bcsg66@silicon.ac.in>
2025-01-21 19:27:45 +0530

    days3

---
+++ javascript_mastery/third.js
@@ -0,0 +1,19 @@
+//boolean
+let isCool=true;
+// if(isCool){
+//   console.log("hi man you're cool");
+// }
+// else{
+//     console.log("Ohhh..hi");
+// }
+// let age=null;
+// console.log(age);
+
+const person={
+    name:'john',
+    age:20,
+}
+console.log(person);
+let arr=[1,"hello",2,true];
+console.log(arr);
+//Array is object
