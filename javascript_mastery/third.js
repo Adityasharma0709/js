565c3219e976469c66589f815c9fa4b228efbda5
CSE66 <cse.23bcsg66@silicon.ac.in>
2025-02-03 23:51:51 +0530

    days 4

--- javascript_mastery/third.js
+++ javascript_mastery/third.js
@@ -13,7 +13,22 @@ const person={
     name:'john',
     age:20,
 }
-console.log(person);
+// console.log(person);
 let arr=[1,"hello",2,true];
-console.log(arr);
+// console.log(arr);
 //Array is object
+
+// javascript is dynamically typed languagee
+// let number ="hello world";
+// console.log(number);
+// console.log(typeof number);
+// number =5;
+// console.log(number);
+// console.log(typeof number);
+//strict equality
+    //compare value and datatype
+let a=5;
+let b=5;
+console.log(a===b);
+//loose equality
+    //a==b compare only value
